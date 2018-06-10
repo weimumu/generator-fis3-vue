f33d6138d4eb82a8a1c7a489c8f560ecc4b07a9a
weimumu <934657014@qq.com>
2018-06-11 07:59:21 +0800

    fis3.conf

--- generators/app/templates/fis-conf.js
+++ generators/app/templates/fis-conf.js
@@ -3,7 +3,7 @@ fis.set('project.ignore', [
     'node_modules/**',
     'fis-conf.js',
     'package-lock.json',
-    'npm-debug.log', 
+    'npm-debug.log',
     'components/demo/**',
     '.yo-rc.json',
     '/package.json',
@@ -27,17 +27,17 @@ fis.hook('commonjs', {
 })
 
 // 在当前目录下安装babel-plugin-transform-runtime，babel-runtime 使用polyfill
-var runtime  = require('babel-plugin-transform-runtime');
+var runtime = require('babel-plugin-transform-runtime');
 var parserEs6 = {
     isMod: true,
     rExt: 'js',
     useSameNameRequire: true,
     parser: [
         fis.plugin('babel-6.x', {
-            plugins : [runtime]
+            plugins: [runtime]
         }),
         fis.plugin('jdists', {
-            remove: "prod"
+            remove: "prod"  // 用/*<prod>*/ /*</prod>*/ 包住的东西会被删除
         }),
     ]
 }
@@ -47,29 +47,35 @@ fis.match('/modules/**.js', parserEs6);
 fis.match('/widget/**.js', parserEs6);
 fis.match('**.vue:js', parserEs6);
 
+//标记为组件
+fis.match('/node_modules/**.js', {
+    isMod: true,
+    useSameNameRequire: true
+});
+
 // 数据模拟不要模块化了 
 // https://github.com/fex-team/fis3-hook-commonjs
 fis.match("/mock/**.js", {
-	ignoreDependencies: true
+    ignoreDependencies: true
 })
 
 //允许你在 js 中直接 require 文件, 允许你在 js 中直接 require css 文件。
 fis.match('**.{js,jsx,ts,es6,vue}', {
     preprocessor: [
-		fis.plugin('js-require-file'),
-        fis.plugin('js-require-css',{
+        fis.plugin('js-require-file'),
+        fis.plugin('js-require-css', {
             //mode : 'inline'
-        }) 
+        })
     ]
 });
 
 // vue文件里面的scss
 fis.match('**.vue:scss', {
-	rExt: 'css',
-	parser: [
-		fis.plugin('node-sass')
-	],
-    useSprite: true,
+    rExt: 'css',
+    parser: [
+        fis.plugin('node-sass')
+    ],
+    useSprite: true, // css图片合并，只会处理 url 中带 ?__sprite 图片的规则。
     // 标准化处理，加css前缀
     preprocessor: fis.plugin('autoprefixer', {
         // https://www.npmjs.com/package/fis3-preprocessor-autoprefixer
@@ -79,15 +85,50 @@ fis.match('**.vue:scss', {
 
 // sass. scss文件处理
 fis.match('*.{scss,sass}', {
-    // sass编译
-    parser: fis.plugin('node-sass'), //启用fis-parser-sass插件
-    // 产出css后缀的名字
+    parser: fis.plugin('node-sass'),
     rExt: '.css',
-    // 使用雪碧图
     useSprite: true,
     // 标准化处理，加css前缀
     preprocessor: fis.plugin('autoprefixer', {
-        // https://www.npmjs.com/package/fis3-preprocessor-autoprefixer
         "browsers": ["Android >= 2.4", "iOS >= 4", "ie >= 9", "firefox >= 15"]
     })
 });
+
+// css文件处理
+fis.match('**.css', {
+    useSprite: true,
+    preprocessor: fis.plugin('autoprefixer', {
+        "browsers": ["Android >= 2.4", "iOS >= 4", "ie >= 9", "firefox >= 15"]
+    })
+});
+
+fis.match('::package', {
+    // css精灵合并  更多配置  https://github.com/fex-team/fis-spriter-csssprites
+    spriter: fis.plugin('csssprites', {
+        // 图之间的边距
+        margin: 5,
+        // 使用矩阵排列方式，默认为线性`linear`
+        layout: 'matrix',
+        scale: 0.5
+    }),
+    // https://github.com/fex-team/fis3-packager-deps-pack
+    packager: fis.plugin('deps-pack', {
+        useTrack: false,// 是否输出路径信息,默认为 true
+        'pkg/npm.js': [
+            'node_modules/**.js'  // node_modules的模块一般都不变的，所以独立打包
+        ]
+    }),
+    // https://github.com/fex-team/fis3-postpackager-loader
+    postpackager: fis.plugin('loader', {
+        resourceType: 'mod',
+        useInlineMap: true,
+        allInOne: {
+            js: function (file) {
+                return "/pkg/js/" + file.filename + "_aio.js";
+            },
+            css: function (file) {
+                return "/pkg/css/" + file.filename + "_aio.css";
+            }
+        }
+    })
+})
